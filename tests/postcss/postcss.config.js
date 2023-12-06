1a4f9537912238d7866f67d068e15a767a194984
razonyang <razonyang@gmail.com>
2023-12-06 22:38:22 +0800

    Test PostCSS, PurgeCSS, Autoprefixer and RTLCSS

---
+++ tests/postcss/postcss.config.js
@@ -0,0 +1,13 @@
+const autoprefixer = require('/usr/local/lib/node_modules/autoprefixer')
+const rtlcss = require('/usr/local/lib/node_modules/rtlcss')
+const purgecss = require('/usr/local/lib/node_modules/@fullhuman/postcss-purgecss')
+
+module.exports = {
+  plugins: [
+      autoprefixer,
+      purgecss({
+        content: ['index.html']
+      }),
+      rtlcss
+  ]
+}
