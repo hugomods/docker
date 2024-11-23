38b8c47e04190ccfb806192507a41eaa4f88da3a
Razon Yang <razonyang@gmail.com>
2024-11-24 06:46:47 +0800

    fix: correct PurgeCSS tests [build] (#109)

--- tests/postcss/postcss.config.js
+++ tests/postcss/postcss.config.js
@@ -1,6 +1,6 @@
 const autoprefixer = require('/usr/local/lib/node_modules/autoprefixer')
 const rtlcss = require('/usr/local/lib/node_modules/rtlcss')
-const purgecss = require('/usr/local/lib/node_modules/@fullhuman/postcss-purgecss')
+const purgecss = require('/usr/local/lib/node_modules/@fullhuman/postcss-purgecss').default
 
 module.exports = {
   plugins: [
